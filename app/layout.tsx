26a88205f03ef6c6fcb03ee4ea8e0468e36ab830
no3dhalparagp <no3dhalparagp@gmail.com>
2025-08-06 22:52:01 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,7 +63,7 @@ export default async function RootLayout({ children }: Props) {
 
                 
                   <main>
-                    <IndianClock />
+                    
                     {children}
                   </main>
               
