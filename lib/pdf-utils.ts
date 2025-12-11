50557b43a8b8dc73f134cc8b53a708186e753dfe
no3dhalparagp <no3dhalparagp@gmail.com>
2025-12-11 22:08:44 +0530

    Update pdf-utils.ts

--- lib/pdf-utils.ts
+++ lib/pdf-utils.ts
@@ -10,6 +10,7 @@
  * @param returnFullDataUrl - If true, return the full Data URL; otherwise, return only the Base64 string.
  * @returns A promise that resolves to the Base64 string or Data URL representation of the PDF file.
  */
+import { blockname, gpcode, gpname, nameinprodhan, gpaddress } from "@/constants/gpinfor";
 export async  function convertPdfToBase64(
   file: File,
   returnFullDataUrl: boolean = false
@@ -80,4 +81,71 @@ export async function fetchPdfFromPublicAndConvertToBase64(
   } catch (error) {
     throw new Error(`Error fetching or converting the file`);
   }
+}
+
+import { Workorderdetails } from "@/types/tender-manage";
+import { workorderforward } from "@/constants";
+import { formatDate } from "@/utils/utils";
+import { getworklenthbynitno } from "@/lib/auth";
+import { getBase64FromUrl } from "@/utils";
+
+/**
+ * Transforms a Workorderdetails object into the input format for the work order certificate PDF template.
+ */
+export async function getWorkOrderCertificateInput(workOrderDetails: Workorderdetails) {
+  const getNitYear = (): number => {
+    const memoDate = workOrderDetails.Bidagency?.WorksDetail?.nitDetails?.memoDate;
+    return memoDate ? new Date(memoDate).getFullYear() : new Date().getFullYear();
+  };
+
+  const workorderyear = workOrderDetails.awardofcontractdetails.workordeermemodate.getFullYear() || "";
+
+  const calculateBidPercentage = (): string => {
+    const estimateAmount = workOrderDetails.Bidagency?.WorksDetail?.finalEstimateAmount || 0;
+    const biddingAmount = workOrderDetails.Bidagency?.biddingAmount || 0;
+    if (estimateAmount && biddingAmount && estimateAmount !== 0) {
+      const percentage = ((estimateAmount - biddingAmount) / estimateAmount) * 100;
+      return percentage.toFixed(2);
+    }
+    return "0.00";
+  };
+
+  const createTableData = (): string[][] => {
+    const data = [
+      "1",
+      workOrderDetails.Bidagency?.WorksDetail?.ApprovedActionPlanDetails?.activityDescription,
+      `${workOrderDetails.Bidagency?.WorksDetail?.finalEstimateAmount}`,
+      `${workOrderDetails.Bidagency?.biddingAmount}`,
+      "As per Govt. Norms and latest guideline of Govt.",
+    ];
+    return [data.map((item) => (item || "N/A").toString())];
+  };
+
+  const nitworkcount = await getworklenthbynitno(
+    workOrderDetails.Bidagency?.WorksDetail?.nitDetails.memoNumber || 0,
+    workOrderDetails.Bidagency?.WorksDetail?.nitDetailsId || ""
+  );
+  const logoBase64 = await getBase64FromUrl("/images/logo.png");
+  const bidPercentage = calculateBidPercentage();
+  const table = createTableData();
+
+  return {
+    refno: `${workOrderDetails.awardofcontractdetails?.workodermenonumber || ""}/DGP/${workorderyear}`,
+    gpname: `${gpname}`,
+          gpaddress: `${gpaddress} , ${blockname}, Dakshin Dinajpur`,
+          gpname2: `${nameinprodhan}`,
+          gpname3: `${nameinprodhan}`,
+    logo: logoBase64,
+    refdate: formatDate(workOrderDetails.awardofcontractdetails?.workordeermemodate) || "",
+    agencyname: workOrderDetails.Bidagency?.agencydetails?.name || "",
+    agencyadd: `${workOrderDetails.Bidagency?.agencydetails?.contactDetails || ""} - ${workOrderDetails.Bidagency?.agencydetails.mobileNumber || ""}`,
+    fund: workOrderDetails.Bidagency?.WorksDetail?.ApprovedActionPlanDetails?.schemeName || "",
+    worksl: `${workOrderDetails.Bidagency?.WorksDetail?.workslno || ""} out of ${nitworkcount}`,
+    nitno: `${workOrderDetails.Bidagency?.WorksDetail?.nitDetails?.memoNumber || ""}/DGP/${getNitYear()} ${workOrderDetails.Bidagency?.WorksDetail?.nitDetails?.memoDate ? formatDate(workOrderDetails.Bidagency?.WorksDetail?.nitDetails?.memoDate) : ""}`,
+    workname: `${workOrderDetails.Bidagency?.WorksDetail?.ApprovedActionPlanDetails?.activityDescription || ""}-${workOrderDetails.Bidagency?.WorksDetail?.ApprovedActionPlanDetails?.activityCode || ""}`,
+    body1: `As the rate offered by you for execution of the above mentioned scheme under ${workOrderDetails.Bidagency?.WorksDetail?.ApprovedActionPlanDetails?.schemeName || ""} fund, invited vide above NIT is found to be the 1st lowest, also in view of the agreement executed by you on ${formatDate(workOrderDetails.awardofcontractdetails?.workordeermemodate) || ""} for accomplishing the proposed consolidated work, following are the stipulated terms and conditions and the work order is hereby issued for execution of work at the accepted rate which is ${bidPercentage}% less than the NIT Tendered Amount.`,
+    body2: "Entire work will have to be completed under the effective and technical guidance of Nirman Sahayak of Gram Panchayat. The said work shall have to be completed within 30(Thirty) days from the date of receiving the work order.",
+    table: table,
+    forwardtable: workorderforward.map((term, i) => [`${i + 1}. ${term}`]) || [],
+  };
 }
