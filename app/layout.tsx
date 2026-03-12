56fe8483fbe060730ee8fdfef97a33746c22005b
no3dhalparagp <no3dhalparagp@gmail.com>
2026-03-12 15:32:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default async function RootLayout({ children }: Props) {
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
