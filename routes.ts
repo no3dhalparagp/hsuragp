b77e02b3f5386218d3a273e19e9b1e67d93684eb
no3dhalparagp <no3dhalparagp@gmail.com>
2026-03-30 10:32:08 +0530

    gs

--- routes.ts
+++ routes.ts
@@ -29,8 +29,8 @@ export const apiAuthPrefix = "/api/auth";
  *@type {string}
  */
 
-// All roles redirect to unified /dashboard
 export const DEFAULT_LOGIN_REDIRECT = "/dashboard/home";
-export const DEFAULT_ADMINLOGIN_REDIRECT = "/dashboard/home";
-export const DEFAULT_STAFFLOGIN_REDIRECT = "/dashboard/home";
-export const DEFAULT_SUPERADMINLOGIN_REDIRECT = "/dashboard/home";
+export const DEFAULT_ADMINLOGIN_REDIRECT = "/admindashboard/home";
+export const DEFAULT_STAFFLOGIN_REDIRECT = "/employeedashboard/home";
+export const DEFAULT_AGENCYLOGIN_REDIRECT = "/agencydashboard";
+export const DEFAULT_SUPERADMINLOGIN_REDIRECT = "/superadmindashboard";
